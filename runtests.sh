b289918e49b4d5d4ed026edd6eeee785093a04a6
Simone Gentili <sensorario@gmail.com>
2017-09-17 13:33:58 +0200

    add scritp to run all creational tests

---
+++ runtests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go test creational/abstract_factory/*.go
+go test creational/builder/*.go
+go test creational/factory/*.go
+go test creational/pool/*.go
+go test creational/prototype/*.go
+go test creational/singleton/*.go
